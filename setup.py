94ea13003ff1db37ee6eedcde5af99b4a3572e63
Justin Van Winkle <justin@milo.com>
2011-10-19 19:59:31 -0700

    initial try

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='flexiconfig',
+      version='1.0',
+      description='Flexible Python Configuration',
+      author='Justin Van Winkle',
+      author_email='justin.vanwinkle@gmail.com',
+      url='https://github.com/justinvanwinkle/flexiconfig',
+      packages=['flexiconfig'])
